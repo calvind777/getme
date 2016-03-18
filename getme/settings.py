5638f5b914b4047a96e54cb592b30b8b529f0a6f
Calvin Dong <calvinldong@gmail.com>
2016-03-18 01:02:26 -0700

    deployment settings

--- getme/settings.py
+++ getme/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'va^hp)85=7f74041c0zuqvjsga8wah=#-s$3d)nw95wfwj-)27'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-#ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 
 
 
