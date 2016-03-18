313cf431eeab5b1d7d9a4a19975d897f217ab3ff
Calvin Dong <calvinldong@gmail.com>
2016-03-17 22:22:06 -0700

    deployment prepare

--- getme/settings.py
+++ getme/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'getme.urls'
 
-
+STATIC_ROOT = “/getme/static“
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
